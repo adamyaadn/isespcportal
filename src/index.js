f8e56600360f5c826a581d027ff1499e03a6c109
adamyaadn <adamyaadn14@gmail.com>
2021-05-09 19:46:44 +0530

    add: initialCommit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,112 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import styled from 'styled-components'
+
+
+// const Button = styled.button
+
+function App() {
+  return (
+    <header>
+        
+       <img src="http://upload.wikimedia.org/wikipedia/en/a/a7/RVCE_New_Logo.JPG" className="App-logo" alt="logo" />
+       <div className="xyz">
+        ISE SPC PORTAL
+      </div>
+    
+    <br></br>
+    <br></br>
+
+    
+    <div className = "Links">
+    <p>
+    <a href="https://docs.google.com/spreadsheets/d/1HUY9TfB5Ew_OKzci_ge6jJs0lml7LxtKB00QzDq8WZs/edit?usp=sharing" target="_blank" rel="noreferrer">
+    ISE Attendance Sheet 2021 (6th semester)
+    </a></p>
+      
+    
+
+    <p>
+    <a href="https://drive.google.com/file/d/1mfOS9SiCoVNz08LngtxwYvg6GQhGEF2m/view?usp=sharing" target="_blank" rel="noreferrer">
+    ISE Attendance Sheet 2021 (5th semester)
+    </a>
+    </p>
+      
+    <p>
+    <a href="https://docs.google.com/spreadsheets/d/1Kd-n-DHAkBN1Mhf2HggDMXO8AS2LfM3drrlUBHLoUqE/edit?usp=sharing" target="_blank" rel="noreferrer">
+    ISE Student Database
+    </a>
+    </p>
+
+    <p>
+    <a href="https://docs.google.com/spreadsheets/d/1QxNHbX5790bd-06geW450wI3lhlHyRy8suoisMhu8xI/edit?usp=sharing" target="_blank" rel="noreferrer">
+    ISE 2022 Students List 
+    </a>
+    </p>
+
+    <p>
+    <a href="https://docs.google.com/spreadsheets/d/1og--ufH9lsnngU7qMJh30HNM8ypwscc4DnRxMme3cos/edit?usp=sharing" target="_blank" rel="noreferrer">
+    ISE 5th semester CGPA List
+    </a>
+    </p>
+   
+    <p>
+    <a href="https://drive.google.com/file/d/1BTRfXb56CRUQ44dpgyypHNmtxxXfvZDd/view?usp=sharing" target="_blank" rel="noreferrer">
+    ISE Internship Drive 2022
+    </a>
+    </p> 
+
+    <p>
+    <a href="https://drive.google.com/file/d/19fK528sndZBM357BGjSMpZjesCjg2FUs/view?usp=sharing" target="_blank" rel="noreferrer">
+    ISE Internship Drive 2022 - Company Wise
+    </a>
+    </p>
+    
+
+    <p>
+    <a href="https://docs.google.com/spreadsheets/d/1Gs9hny-uKlFSMRABAIaRLU5fpw4j85-0I6YQkV_jVW4/edit?usp=sharing" target="_blank" rel="noreferrer">
+    Internship Drive 2022 - All Branches
+    </a>
+    </p>
+    
+
+    <p>
+    <a href="https://drive.google.com/file/d/18DnduN5Ag4MkSeY9fj0FFcYzOO7mJrz-/view?usp=sharing" target="_blank" rel="noreferrer">
+    Internship Start and End Dates - ISE
+    </a>
+    </p>
+    
+
+    <p>
+    <a href="https://forms.gle/1BH5C2K3KSC4pvzX6" target="_blank" rel="noreferrer">
+    Company Registration Form Template
+    </a>
+    </p>
+   
+
+    <p>
+    <a href="https://docs.google.com/document/d/1e2CEKJtt9ZzPYFsR7SMa2Mm4zIIVYYgyea7bQcOw6zc/edit?usp=sharing" target="_blank" rel="noreferrer">
+    ISE 2022 Internship Drive Experience
+    </a>
+    </p>
+    
+
+    <p>
+    <a href="https://drive.google.com/drive/folders/1CZaeK6gKdqHbYQbjf4Wk7CoXU1NRMYFw?usp=sharing" target="_blank" rel="noreferrer">
+    Offer acceptance letters drive
+    </a>
+    </p>
+   
+
+    </div>
+
+    </header>
+  );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
